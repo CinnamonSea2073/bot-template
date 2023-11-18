aa904418602220ad81702097f4520aae570be40c
CinnamonSea2073 <Keimonn0329@gmail.com>
2023-11-19 08:53:38 +0900

    改良

--- bot/cogs/hoge.py
+++ bot/cogs/hoge.py
@@ -2,18 +2,20 @@ import discord
 from discord.ext import commands 
 from discord.commands import Option, OptionChoice, SlashCommandGroup
 
-
-class Hoge(commands.Cog):
+# クラス名にCogと付けたほうが分かりやすい
+class HogeCog(commands.Cog):
 
     def __init__(self, bot:commands.bot):
         print(f"init -> {self.__class__}")
         self.bot = bot
 
+    # nameは全て小文字
     group = SlashCommandGroup(name="name", description="description")
 
+    # コマンド名は全て小文字。関数名の重複に注意。
     @group.command(name="ping", description="通信テスト")
     async def ping(self, ctx: discord.ApplicationContext):
         await ctx.response.send_message(content="pong!")
 
 def setup(bot: commands.bot):
-    bot.add_cog(Hoge(bot))
+    bot.add_cog(HogeCog(bot))
--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,8 @@ import os
 import asyncio
 
 intents = discord.Intents.default()
-intents.guilds = True
+# 特権インテントを与える例
+# intents.guilds = True
 bot = commands.AutoShardedBot(intents=intents)
 TOKEN = os.getenv("TOKEN")
 path = "./cogs"
@@ -17,6 +18,9 @@ async def on_application_command_error(ctx: discord.ApplicationContext, error):
     else:
         raise error
 
+@bot.event
+async def on_guild_join(guild: discord.Guild):
+    print(f"新規導入サーバー: {guild.name}")
 
 @bot.event
 async def on_ready():
@@ -27,5 +31,6 @@ bot.load_extensions(
     'cogs.hoge',
     store=True
 )
+# store=Falseにすると、Cogでエラーが発生していた際にクリティカル警告となる
 
 bot.run(TOKEN)
